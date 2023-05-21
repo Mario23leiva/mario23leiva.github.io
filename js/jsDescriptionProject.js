d01dd02486585b1ac769c0b7cb6d0593ecd39888
Mario23leiva <mleiva@alumno.santoangel.org>
2023-05-21 14:30:15 +0200

    o

--- js/jsDescriptionProject.js
+++ js/jsDescriptionProject.js
@@ -1,15 +1,32 @@
+window.onload = function() {
+  cargarProyecto();
+}
+
 function cargarProyecto(carta) {
-    // Obtener el índice de la carta
-    const indice = parseInt(carta.getAttribute('data-indice'));
-  
-    // Cargar el archivo JSON
-    fetch('../json/proyectos/proyectos.json')
-      .then(response => response.json())
-      .then(data => {
-        // Obtener la información de la carta utilizando el índice
-        const cartaSeleccionada = data[indice];
-  
-        // Redirigir a la página de la carta con la información de la carta en la URL
-        window.location.href = `../pages/projects.html?info=${JSON.stringify(cartaSeleccionada)}`;
+    // Obtener el valor del parámetro "info" desde la URL
+  const parametros = new URLSearchParams(window.location.search);
+  const indice = parseInt(parametros.get('info'));
+
+
+  // Cargar el archivo JSON y mostrar el elemento correspondiente
+  fetch('../json/proyectos/proyectos.json')
+    .then(response => response.json())
+    .then(data => {
+      const elemento = data[indice];
+      // Aquí puedes mostrar el elemento en la página
+      const tituloProyecto = document.getElementById('tituloProyecto');
+      tituloProyecto.innerHTML = elemento.titulo;
+      const tipoProyecto = document.getElementById('tipoProyecto');
+      tipoProyecto.innerHTML = elemento.tipo;
+      const imagenProyecto = document.getElementById('imagenProyecto');
+      imagenProyecto.src = elemento.ruta_imagen;
+      const descripcionProyecto = document.getElementById('descripcionProyecto');
+      descripcionProyecto.innerHTML = elemento.descripcion;
+      const tecnologiasProyecto = document.getElementById('tecnologiasProyecto');
+      tecnologiasProyecto.innerHTML = elemento.tecnologias;
+      const moreInfoProyecto = document.getElementById('btn-moreInfo');
+      moreInfoProyecto.addEventListener('click', () => {
+        window.location.href = elemento.url_repo;
+      });
     });
 }
