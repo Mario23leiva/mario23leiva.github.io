17b581ce368e619e9784024dadc3871c117fdcaf
Mario23leiva <mleiva@alumno.santoangel.org>
2023-05-31 20:35:13 +0200

    asd

--- js/jsContact.js
+++ js/jsContact.js
@@ -13,3 +13,29 @@ function cambiarFondo() {
         fondoActual = "url('../multimedia/img/fondoMinecraft.jpg')";
     }
 }
+
+    function enviarFormulario() {
+    event.preventDefault(); // Evita que se envíe el formulario por defecto
+
+    // Obtiene los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    // Configura el objeto de correo electrónico
+    var templateParams = {
+        from_name: nombre,
+        from_email: email,
+        message: mensaje
+    };
+
+    // Envía el correo electrónico
+    emailjs.send('service_zutakp1', 'template_53bi9xb', templateParams)
+        .then(function(response) {
+        console.log('Correo electrónico enviado:', response);
+        alert('Correo electrónico enviado correctamente.');
+        }, function(error) {
+        console.error('Error al enviar el correo electrónico:', error);
+        alert('Ocurrió un error al enviar el correo electrónico.');
+        });
+}
